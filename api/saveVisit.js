2208dea3e946bbe273a94528071170c171dc8295
amal_yuldashev <amalyuldashev@gmail.com>
2025-03-19 21:09:26 +0100

    small changes

--- api/saveVisit.js
+++ api/saveVisit.js
@@ -16,11 +16,7 @@ export default async function handler(req, res) {
         [session_id, visit_start, referrer]
       );
 
-      const visitor_id = result[0].id;
-
-      return res
-        .status(200)
-        .json({ message: "Visit data saved successfully", visitor_id });
+      return res.status(200).json({ message: "Visit data saved successfully" });
     } catch (error) {
       console.error("Error saving visit data:", error);
       return res.status(500).json({ message: "Error saving visit data" });
