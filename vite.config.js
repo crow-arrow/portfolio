61a544cac4a11078b3b1d244ef0359ac4234301d
amal_yuldashev <amalyuldashev@gmail.com>
2025-03-30 00:35:43 +0100

    webp added to vite.config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default {
         description: "My App description",
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,woff2,svg,avif,png,jpg}"],
+        globPatterns: ["**/*.{js,css,html,woff2,svg,avif,webp,png,jpg}"],
       },
     }),
   ],
